d1c292e063ae2b6782ca41cb729afea2afacccd8
Vinny Bass <raylee558@gmail.com>
2020-05-24 17:30:48 -0300

    Pass all tests

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -20,22 +20,34 @@ interface FileProps {
 
 const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(file => {
+      data.append('file', file.file, file.name);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      });
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesToUpload = files.map<FileProps>(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+    });
+    setUploadedFiles(filesToUpload);
   }
 
   return (
